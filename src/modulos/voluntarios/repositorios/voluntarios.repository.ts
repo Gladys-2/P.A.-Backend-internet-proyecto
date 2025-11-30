1b6e20a4be872ae79847ec171b7663ad849db42f
Gladys-2 <gladysninaquispe7@gmail.com>
2025-11-30 01:25:27 -0400

    chore: se actualizó el repositorio de Voluntario para manejo de base de datos

--- src/modulos/voluntarios/repositorios/voluntarios.repository.ts
+++ src/modulos/voluntarios/repositorios/voluntarios.repository.ts
@@ -1,5 +1,5 @@
 import { AppDataSource } from "../../../config/basedatos";
 import { Voluntario } from "../entidades/voluntario.entity";
 import { Repository } from "typeorm";
-
+// Repositorio para manejar la entidad Voluntario (CRUD y consultas a la base de datos)
 export const voluntarioRepository: Repository<Voluntario> = AppDataSource.getRepository(Voluntario);
