fc7eeb6c9b9fc24339440887b6842c9312d3173b
Gladys-2 <gladysninaquispe7@gmail.com>
2025-11-30 01:20:49 -0400

    feat: se agregó fecha de nacimiento y fecha de ingreso a voluntarios

--- src/modulos/voluntarios/entidades/voluntario.entity.ts
+++ src/modulos/voluntarios/entidades/voluntario.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert } from "typeorm";
+import moment from "moment-timezone";
 
 @Entity("voluntarios")
 export class Voluntario {
@@ -14,15 +15,37 @@ export class Voluntario {
   @Column({ type: "varchar", length: 100 })
   apellido_materno: string | undefined;
 
+  @Column({ type: "date", nullable: true })
+  fechaNacimiento?: Date;
+
   @Column({ type: "varchar", length: 20, nullable: true })
   telefono: string | undefined;
 
   @Column({ type: "varchar", length: 150, unique: true })
   correo_electronico: string | undefined;
 
-  @Column({ type: "varchar", length: 150, nullable: true })
+  @Column({ type: "varchar", length: 250, nullable: true })
   direccion: string | undefined;
+
+  @Column({ type: "timestamp", nullable: true })
+  fechaIngreso: Date | undefined;
+
+  @Column({ type: "varchar", length: 100, nullable: true })
+  areaAsignada: string | undefined;
+
+  @Column({ type: "varchar", length: 100, nullable: true })
+  disponibilidad: string | undefined;
+
+  @Column({ type: "text", nullable: true })
+  observaciones: string | undefined;
 
   @Column({ type: "varchar", length: 20, default: "Activo" })
   estado: "Activo" | "Inactivo" | undefined;
+
+  @BeforeInsert()
+  setFechaIngreso() {
+    if (!this.fechaIngreso) {
+      this.fechaIngreso = moment().tz("America/La_Paz").toDate();
+    }
+  }
 }
