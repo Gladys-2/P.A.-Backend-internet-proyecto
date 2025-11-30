6e961de0e6dad9e5b2d9672b184bd3acb4191f8f
Gladys-2 <gladysninaquispe7@gmail.com>
2025-11-30 00:52:09 -0400

    se creo el repositorio de refugios para manejar las consultas a la base de datos

--- src/modulos/refugios/repositorios/refugios.repository.ts
+++ src/modulos/refugios/repositorios/refugios.repository.ts
@@ -2,3 +2,5 @@ import { AppDataSource } from "../../../config/basedatos";
 import { Refugio } from "../entidades/refugio.entity";
 
 export const refugiosRepository = AppDataSource.getRepository(Refugio);
+            //operaciones Crud                                     // obtiene el repositorio de la entidad refugio
+// repository -->controlador listo para usar
