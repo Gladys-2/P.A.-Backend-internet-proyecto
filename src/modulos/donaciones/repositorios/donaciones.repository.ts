e2932d3664a5c57b61dfa2f999edc34f81835423
Gladys-2 <gladysninaquispe7@gmail.com>
2025-11-30 00:39:11 -0400

    se creo el repositorio para manejar las donaciones

--- src/modulos/donaciones/repositorios/donaciones.repository.ts
+++ src/modulos/donaciones/repositorios/donaciones.repository.ts
@@ -2,3 +2,5 @@ import { AppDataSource } from "../../../config/basedatos";
 import { Donacion } from "../entidades/donacion.entity";
 
 export const donacionesRepository = AppDataSource.getRepository(Donacion);
+           //operaciones Crud                                     // obtiene el repositorio de la entidad animal
+// repository -->controlador listo para usar
