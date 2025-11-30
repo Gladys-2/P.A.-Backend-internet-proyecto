b37c12d79e817c20eb937918166e7da52abb0bb3
Gladys-2 <gladysninaquispe7@gmail.com>
2025-11-30 00:43:19 -0400

    se creo el repositorio para manejar los animales en la base de datos

--- src/modulos/animales/repositorios/animales.repository.ts
+++ src/modulos/animales/repositorios/animales.repository.ts
@@ -2,5 +2,5 @@ import { AppDataSource } from "../../../config/basedatos";
 import { Animal } from "../entidades/animal.entity";
 
 export const animalesRepository = AppDataSource.getRepository(Animal);
-           //operaciones Crud                                     // obtiene el repositorio de la entidad adopcion
+           //operaciones Crud                                     // obtiene el repositorio de la entidad animal
 // repository -->controlador listo para usar
