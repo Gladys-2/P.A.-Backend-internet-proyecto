1aa35400bf3db5ded14980a5f01882048693ba7f
Gladys-2 <gladysninaquispe7@gmail.com>
2025-11-30 00:25:13 -0400

    feat: crear repositorio de animales para asi  manejar consultas CRUD

--- src/modulos/animales/repositorios/animales.repository.ts
+++ src/modulos/animales/repositorios/animales.repository.ts
@@ -2,3 +2,5 @@ import { AppDataSource } from "../../../config/basedatos";
 import { Animal } from "../entidades/animal.entity";
 
 export const animalesRepository = AppDataSource.getRepository(Animal);
+           //operaciones Crud                                     // obtiene el repositorio de la entidad adopcion
+// repository -->controlador listo para usar
