b89d92226cffa10694d31ea51f44e94bc461f22d
Gladys-2 <gladysninaquispe7@gmail.com>
2025-11-30 01:08:05 -0400

    Se agregaron las rutas para asignar y listar roles de usuarios

--- src/modulos/usuarios/rutas/usuarioRol.routes.ts
+++ src/modulos/usuarios/rutas/usuarioRol.routes.ts
@@ -7,11 +7,9 @@ import {
 } from "../controladores/roles.controller";
 
 const router = Router();
-
-// Rutas de roles
 router.post("/crear-rol", crearRolController);
 router.get("/listar-roles", obtenerRolesController);
 router.put("/actualizar-rol/:id", actualizarRolController);
 router.delete("/eliminar-rol/:id", eliminarRolController);
 
-export default router;
+export default router;// rutas de usuarioRol
