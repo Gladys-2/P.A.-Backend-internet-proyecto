cebb1eb83babcabbe401e8b81c4f89204c29953a
Gladys-2 <gladysninaquispe7@gmail.com>
2025-11-29 23:39:16 -0400

    actualizo la función para registrar auditoría usando AppDataSource

--- src/core/entidades/registrarAuditoria.ts
+++ src/core/entidades/registrarAuditoria.ts
@@ -1,24 +1,24 @@
-import { getRepository } from "typeorm";
+import { AppDataSource } from "../../config/basedatos";
 import { Auditoria } from "../entidades/auditoria.entity";
 
 /**
- * Su funcion es el registrar acciones en la tabla Auditoria
+ * se basara en el registro de las acciones importantes en la tabla Auditoria
  * @param usuario 
  * @param accion 
  */
 export const registrarAuditoria = async (usuario: string, accion: string) => {
   try {
-    const auditoriaRepo = getRepository(Auditoria);
+    const auditoriaRepo = AppDataSource.getRepository(Auditoria); // <-- cambio aquí
 
     const nuevoRegistro = auditoriaRepo.create({
-      usuario_creacion: usuario,
-      accion: accion,
-      estado: "Activo",
+      usuario_creacion: usuario, // aqui se sabe que es lo que realizo el usuario 
+      accion: accion,// la accion que realizo 
+      estado: "Activo",// el estado de registro activo por defecto
     });
 
     await auditoriaRepo.save(nuevoRegistro);
-    console.log("la auditoria ya fue registrada");
+    console.log("La auditoría ya fue registrada");
   } catch (error) {
-    console.error("Salio error al registrar la auditoria", error);
+    console.error("Salió error al registrar la auditoría", error);
   }
 };
