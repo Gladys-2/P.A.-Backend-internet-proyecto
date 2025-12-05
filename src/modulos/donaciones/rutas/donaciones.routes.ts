5f94f0debdd4da878972a119d1f66b76df30f588
Gladys-2 <gladysninaquispe7@gmail.com>
2025-12-05 15:08:54 -0400

    fix: corregir rutas de donaciones para que el conteo se actualice al instante

--- src/modulos/donaciones/rutas/donaciones.routes.ts
+++ src/modulos/donaciones/rutas/donaciones.routes.ts
@@ -7,10 +7,9 @@ import {
 } from "../controladores/donaciones.controller";
 
 const router = Router();
-
 router.post("/crear-donacion", crearDonacionController);
 router.get("/", obtenerDonacionesController);
 router.put("/actualizar-donacion/:id", actualizarDonacionController);
 router.delete("/eliminar-donacion/:id", eliminarDonacionController);
-
+ 
 export default router; //rutas de donaciones
