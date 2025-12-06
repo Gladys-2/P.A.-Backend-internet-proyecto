aec494eb51bd7fa8704f55ed720e7186cef4c43a
Gladys-2 <gladysninaquispe7@gmail.com>
2025-12-06 05:30:51 -0400

    feat:se refactorizo las rutas de usuarios con control de acceso

--- src/modulos/usuarios/rutas/usuarios.routes.ts
+++ src/modulos/usuarios/rutas/usuarios.routes.ts
@@ -7,12 +7,17 @@ import {
   eliminarUsuarioController,
 } from "../controladores/usuarios.controller";
 
+import { authenticateJWT, esAdmin } from "../../../core/middleware/auth.middleware";
+
 const router = Router();
 
+// Registro (no necesita token)
 router.post("/registro", crearUsuarioController);
-router.get("/", obtenerUsuariosController);
-router.get("/:id", obtenerUsuarioPorIdController);
-router.put("/:id", actualizarUsuarioController);
-router.delete("/:id", eliminarUsuarioController);
+
+// Rutas protegidas
+router.get("/", authenticateJWT, esAdmin, obtenerUsuariosController);
+router.get("/:id", authenticateJWT, esAdmin, obtenerUsuarioPorIdController);
+router.put("/:id", authenticateJWT, esAdmin, actualizarUsuarioController);
+router.delete("/:id", authenticateJWT, esAdmin, eliminarUsuarioController);
 
 export default router;
